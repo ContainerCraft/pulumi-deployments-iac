59eab034bc9628301eb0057028bf20f7112757bd
Kat Morgan <kat@braincraft.io>
2024-05-14 18:17:48 +0000

    update

--- pulumi/src/pulumi/deploy.py
+++ pulumi/src/pulumi/deploy.py
@@ -9,53 +9,77 @@ def pulumi_cloud_deployment(
         repository_name: str
     ):
 
-    # Pulumi Cloud: Deployment Configuration
-    pulumi_cloud_deployment_config = pulumiservice.DeploymentSettings(
-        f"{organization_name}/{project_name}/{stack_name}",
-        stack=stack_name,
-        project=project_name,
-        organization=organization_name,
+    pulumiservice_deploymentsettings = pulumiservice.DeploymentSettings("pulumiservice_deploymentsettings",
+        agent_pool_id="",
+        executor_context=pulumiservice.DeploymentSettingsExecutorContextArgs(
+            executor_image="pulumi/pulumi",
+        ),
         github=pulumiservice.DeploymentSettingsGithubArgs(
-            repository=f"{organization_name}/{repository_name}",
             deploy_commits=True,
             preview_pull_requests=True,
-            pull_request_template=True,
-            paths=["."]
+            pull_request_template=False,
+            repository="ContainerCraft/pulumi-deployments-iac",
         ),
+        operation_context=pulumiservice.DeploymentSettingsOperationContextArgs(
+            options=pulumiservice.OperationContextOptionsArgs(),
+        ),
+        organization="ContainerCraft",
+        project="kubernetes-platform",
         source_context=pulumiservice.DeploymentSettingsSourceContextArgs(
             git=pulumiservice.DeploymentSettingsGitSourceArgs(
                 branch="main",
-                repo_dir="."
-            )
-        ),
-        executor_context=pulumiservice.DeploymentSettingsExecutorContextArgs(
-            executor_image="docker.io/pulumi/pulumi"
-        ),
-        operation_context=pulumiservice.DeploymentSettingsOperationContextArgs(
-            options=pulumiservice.OperationContextOptionsArgs(
-                delete_after_destroy=False,
-                shell="/bin/bash",
-                skip_install_dependencies=False,
-                skip_intermediate_deployments=False
             ),
-            #environment_variables={
-            #    "PULUMI_ACCESS_TOKEN": "YOUR_ACCESS_TOKEN",  # Ensure to secure or inject this securely
-            #    "PULUMI_ORG": organization_name,
-            #    "PULUMI_PROJECT": project_name,
-            #    "PULUMI_STACK": stack_name
-            #},
-            #pre_run_commands=[
-            #    "pip install -r requirements.txt",
-            #    "pulumi up --yes"
-            #]
         ),
-        agent_pool_id="",
-        opts=pulumi.ResourceOptions(
-            protect=False
-        )
-    )
+        stack="dev",
+        opts=pulumi.ResourceOptions(protect=False))
 
-    return pulumi_cloud_deployment_config
+    ## Pulumi Cloud: Deployment Configuration
+    #pulumi_cloud_deployment_config = pulumiservice.DeploymentSettings(
+    #    f"{organization_name}/{project_name}/{stack_name}",
+    #    stack=stack_name,
+    #    project=project_name,
+    #    organization=organization_name,
+    #    github=pulumiservice.DeploymentSettingsGithubArgs(
+    #        repository=f"{organization_name}/{repository_name}",
+    #        deploy_commits=True,
+    #        preview_pull_requests=True,
+    #        pull_request_template=True,
+    #        paths=["."]
+    #    ),
+    #    source_context=pulumiservice.DeploymentSettingsSourceContextArgs(
+    #        git=pulumiservice.DeploymentSettingsGitSourceArgs(
+    #            branch="main",
+    #            repo_dir="."
+    #        )
+    #    ),
+    #    executor_context=pulumiservice.DeploymentSettingsExecutorContextArgs(
+    #        executor_image="docker.io/pulumi/pulumi"
+    #    ),
+    #    operation_context=pulumiservice.DeploymentSettingsOperationContextArgs(
+    #        options=pulumiservice.OperationContextOptionsArgs(
+    #            delete_after_destroy=False,
+    #            shell="/bin/bash",
+    #            skip_install_dependencies=False,
+    #            skip_intermediate_deployments=False
+    #        ),
+    #        #environment_variables={
+    #        #    "PULUMI_ACCESS_TOKEN": "YOUR_ACCESS_TOKEN",  # Ensure to secure or inject this securely
+    #        #    "PULUMI_ORG": organization_name,
+    #        #    "PULUMI_PROJECT": project_name,
+    #        #    "PULUMI_STACK": stack_name
+    #        #},
+    #        #pre_run_commands=[
+    #        #    "pip install -r requirements.txt",
+    #        #    "pulumi up --yes"
+    #        #]
+    #    ),
+    #    agent_pool_id="",
+    #    opts=pulumi.ResourceOptions(
+    #        protect=False
+    #    )
+    #)
+
+    return pulumiservice_deploymentsettings
 
 #import pulumi
 #import pulumi_pulumiservice as pulumiservice
